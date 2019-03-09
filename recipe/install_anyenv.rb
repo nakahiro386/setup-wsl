97b4ed9fdc0d5ad075a8d2503eee882bf3392580
nakahiro386 <nakahiro386@gmail.com>
2019-03-09 11:08:02 +0900

    envによるインストールとglobalの設定を追加

--- recipe/install_anyenv.rb
+++ recipe/install_anyenv.rb
@@ -8,20 +8,38 @@ node[:target_group] ||= run_command("id -gn #{node[:target_user]}").stdout.strip
 node[:user_home] ||= user_info['directory']
 node[:anyenv_root] ||= "#{node[:user_home]}/.anyenv"
 
+directory "#{node[:user_home]}/.local" do
+  action :create
+  mode "700"
+end
+directory "#{node[:user_home]}/.local/bin" do
+  action :create
+  mode "775"
+end
+
 git node[:anyenv_root] do
   repository "https://github.com/anyenv/anyenv"
+  not_if "test -d #{node[:anyenv_root]}"
 end
 
 node[:profile] ||= "#{node[:user_home]}/.profile"
 file "#{node[:profile]}" do
   action :edit
   block do |content|
-    unless content =~ /anyenv init/
+    unless content =~ /ANYENV_ROOT/
       content.concat <<-CONF
 # mitamae managed
-export PATH="#{node[:anyenv_root]}/bin:$PATH"
-eval "$(anyenv init -)"
-
+if [ -x "#{node[:anyenv_root]}/bin/anyenv" ] ; then
+    export ANYENV_ROOT=#{node[:anyenv_root]};
+    export PATH="$ANYENV_ROOT/bin:$PATH"
+    if [ -r "#{node[:user_home]}/.anyenv_init" ] ; then
+        source "#{node[:user_home]}/.anyenv_init"
+    else
+        anyenv init - bash --no-rehash > "#{node[:user_home]}/.anyenv_init"
+        eval "$(anyenv init - bash)"
+    fi
+fi
+export PIPENV_VENV_IN_PROJECT=true
       CONF
     end
   end
@@ -32,38 +50,71 @@ execute  "#{node[:anyenv_root]}/bin/anyenv install --force-init" do
 end
 
 node[:anyenv_plugins_dir] ||= "#{node[:anyenv_root]}/plugins"
-node[:anyenv_plugins] ||= %w(anyenv-update anyenv-git)
+node[:anyenv_plugins] ||= [
+  'https://github.com/znz/anyenv-update.git',
+  'https://github.com/znz/anyenv-git.git',
+]
 node[:anyenv_plugins].each do |plugin|
-  git "#{node[:anyenv_plugins_dir]}/#{plugin}" do
-    repository "https://github.com/znz/#{plugin}.git"
+  plugin_name = File.basename(plugin).gsub(/\.git$/, '')
+  plugin_dir = "#{node[:anyenv_plugins_dir]}/#{plugin_name}"
+  git plugin_dir do
+    repository plugin
+    not_if "test -d #{plugin_dir}"
   end
 end
 
 node[:anyenv_envs] ||= {
-  pyenv: [
-    'https://github.com/pyenv/pyenv-doctor.git',
-    'https://github.com/pyenv/pyenv-installer.git',
-    'https://github.com/pyenv/pyenv-update.git',
-    'https://github.com/pyenv/pyenv-virtualenv.git',
-    'https://github.com/pyenv/pyenv-which-ext.git'
-  ],
-  rbenv: [
-    'https://github.com/sstephenson/rbenv-default-gems.git',
-    'https://github.com/sstephenson/rbenv-gem-rehash.git'
-  ]
+  pyenv: {
+    'plugins': [
+      'https://github.com/momo-lab/xxenv-latest.git',
+      'https://github.com/pyenv/pyenv-doctor.git',
+      'https://github.com/pyenv/pyenv-installer.git',
+      'https://github.com/pyenv/pyenv-update.git',
+      'https://github.com/pyenv/pyenv-virtualenv.git',
+      'https://github.com/pyenv/pyenv-which-ext.git'
+    ],
+    'versions': [
+      '3.7.2'
+    ],
+    'global': '3.7.2'
+  },
+  rbenv: {
+    'plugins': [
+      'https://github.com/momo-lab/xxenv-latest.git',
+      'https://github.com/sstephenson/rbenv-default-gems.git',
+      'https://github.com/sstephenson/rbenv-gem-rehash.git'
+    ],
+    'versions': [
+    ]
+  }
 }
 
-node[:anyenv_envs].each do |env, plugins|
+node[:anyenv_envs].each do |env, options|
+  env_root = "#{node[:anyenv_root]}/envs/#{env}"
   execute  "#{node[:anyenv_root]}/bin/anyenv install --skip-existing #{env}" do
-    not_if "test -d #{node[:anyenv_root]}/envs/#{env}"
+    not_if "test -d #{env_root}"
   end
-  directory "#{node[:anyenv_root]}/envs/#{env}/cache" do
+  directory "#{env_root}/cache" do
     action :create
   end
-  plugins.each do |plugin|
+  options['plugins'].each do |plugin|
     plugin_name = File.basename(plugin).gsub(/\.git$/, '')
-    git "#{node[:anyenv_root]}/envs/#{env}/plugins/#{plugin_name}" do
+    plugin_dir = "#{node[:anyenv_root]}/envs/#{env}/plugins/#{plugin_name}"
+    git plugin_dir do
       repository plugin
+      not_if "test -d #{plugin_dir}"
+    end
+  end
+  options['versions'].each do |version|
+    execute  "#{env_root}/bin/#{env} install --skip-existing #{version}" do
+      not_if "test -d #{env_root}/versions/#{version}"
+    end
+  end
+  global = options['global']
+  if global
+    execute  "#{env_root}/bin/#{env} global #{global}" do
+      not_if "#{env_root}/bin/#{env} global | grep #{global}"
     end
   end
 end
+
