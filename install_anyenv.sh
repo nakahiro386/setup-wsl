c02c84b95ff20aa5d91a764ccdc45a727a5a0f7e
nakahiro386 <nakahiro386@gmail.com>
2019-07-27 08:04:42 +0900

    PYTHONのconfigureオプションに--enable-sharedを追加

--- install_anyenv.sh
+++ install_anyenv.sh
@@ -6,5 +6,7 @@ mitamae_version="mitamae-${version}"
 mitamae_path="./bin/${mitamae_version}"
 
 sudo ${mitamae_path} local $@ recipe/install_anyenv_package.rb
+
+export PYTHON_CONFIGURE_OPTS="--enable-shared"
 ${mitamae_path} local $@ recipe/install_anyenv.rb
 
